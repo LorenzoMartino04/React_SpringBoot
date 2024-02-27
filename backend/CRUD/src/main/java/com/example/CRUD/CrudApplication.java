b12038765578dd3e900da0f491ded7ac12be1e2c
ElGnappo <66874823+ElGnappo@users.noreply.github.com>
2024-02-27 02:24:56 +0100

    commit

--- backend/CRUD/src/main/java/com/example/CRUD/CrudApplication.java
+++ backend/CRUD/src/main/java/com/example/CRUD/CrudApplication.java
@@ -1,13 +1,31 @@
 package com.example.CRUD;
 
+import com.example.CRUD.database.DatabaseConnection;
+import com.example.CRUD.database.DatabaseQuery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
+@RestController
 public class CrudApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
+	@GetMapping("/getUsers")
+	public String users() {
+		DatabaseQuery databaseQuery = new DatabaseQuery();
+		try {
+			return databaseQuery.getAllUsers();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "Error retrieving users.";
+		}
+	}
 }
